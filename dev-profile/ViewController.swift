b41ec1fa13bcf626706b7bdf51f964fefbe476eb
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-06-14 11:10:12 +0700

    first commit

---
+++ dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Muhammad Iqbal Pradipta on 13/06/19.
+//  Copyright © 2019 Muhammad Iqbal Pradipta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
